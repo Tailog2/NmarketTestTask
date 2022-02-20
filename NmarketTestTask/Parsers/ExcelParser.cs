4f63463b5ac3f289408c196995c6907386fa5a28
Lev <nedra.tailog@gmail.com>
2022-02-20 18:43:12 +0300

    Add project files.

---
+++ NmarketTestTask/Parsers/ExcelParser.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ClosedXML.Excel;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class ExcelParser : IParser
+    {
+        private IXLWorksheet sheet;
+        private List<House> houses;
+
+        public IList<House> GetHouses(string path)
+        {
+            houses = new List<House>();
+
+            var workbook = new XLWorkbook(path);
+            sheet = workbook.Worksheets.First();
+
+            // Извлекает ячейки с именами домов
+            var housesInSheet = sheet.Cells().Where(c => c.GetValue<string>().Contains("Дом")).ToList();       
+            AddHouses(housesInSheet);
+
+            return houses;
+        }
+
+        // Принимает список с ячейками домов
+        // Создает новый обект класса House и заполняет его данными
+        // Добавляет новый обект класса House в список houses
+        private void AddHouses(List<IXLCell> housesInSheet)
+        {
+            for (int i = 0; i < housesInSheet.Count; i++)
+            {
+                var house = GetHouse(housesInSheet, i);
+                houses.Add(house);
+            }
+        }
+
+        // Принимает список с ячейками домов и его индекс в списке    
+        // Извлекает ячейку с домом и  ячейку со следующем в списке домом
+        // Если это последний дом в списке присваивает ячейке со следующим домом значение null
+        // Извлекает все картины для дома соответствующего принятому индекс
+        // Создает объект House, заполняет его данными и возращает его
+        private House GetHouse(List<IXLCell> housesInSheet, int houseIndex)
+        {
+
+            var houseInSheet = housesInSheet[houseIndex];
+            IXLCell nextHouseInSheet = null;
+
+            if (houseIndex < housesInSheet.Count - 1)
+                nextHouseInSheet = housesInSheet[houseIndex + 1];
+
+            var flats = GetFlats(houseInSheet, nextHouseInSheet);
+
+            var house = new House()
+            {
+                Name = houseInSheet.GetValue<string>(),
+                Flats = flats
+            };
+
+            return house;
+        }
+
+        // Принимает ячейку с текущим домом и со следующим домом
+        // Извлекает и возвращает все квартиры для текущего дома
+        private List<Flat> GetFlats(IXLCell houseInSheet, IXLCell nextHouseInSheet)
+        {
+            List<IXLCell> flatsInSheet = FindFlats(houseInSheet, nextHouseInSheet);
+            
+            return ConvertFlatsCellsToFlats(flatsInSheet);
+        }
+
+        // Принимает ячейку с текущим домом и со следующим домом
+        // Находит все квартиры для текущим домом
+        // Находятся все квартиры начиная от ячейки текущим домом заканчивая ячейкой следующего дома
+        // Поиск идет сверху вниз построчно
+        // Возвращает список с ячейками квартир для текущего дома
+        private List<IXLCell> FindFlats(IXLCell houseInSheet, IXLCell nextHouseInSheet)
+        {
+            List<IXLCell> flatsInSheet;
+
+            if (nextHouseInSheet != null)
+            {
+                flatsInSheet = sheet.Cells()
+                .Where(c => c.GetValue<string>().Contains("№"))
+                .SkipWhile((cell, index) => cell.WorksheetRow().RowNumber() < houseInSheet.WorksheetRow().RowNumber())
+                .TakeWhile((cell, index) => cell.WorksheetRow().RowNumber() < nextHouseInSheet.WorksheetRow().RowNumber())
+                .ToList();
+            }
+            else
+            {
+                flatsInSheet = sheet.Cells()
+                .SkipWhile((cell, index) => cell.WorksheetRow().RowNumber() < houseInSheet.WorksheetRow().RowNumber())
+                .Where(c => c.GetValue<string>().Contains("№"))
+                .ToList();
+            }
+
+            return flatsInSheet;
+        }
+
+        // Преобразует список с ячейками квартир в список с объектами класса Flat
+        private List<Flat> ConvertFlatsCellsToFlats(List<IXLCell> flatsInSheet)
+        {
+            var flats = new List<Flat>();
+            foreach (var flatInSheet in flatsInSheet)
+            {
+                var flatNumber = GetFlatNumber(flatInSheet);
+                var price = GetPrice(flatInSheet);
+                flats.Add(new Flat() { Number = flatNumber, Price = price });
+            }
+            return flats;
+        }
+
+        // Извлекает и возвращает номер квартиры
+        private string GetFlatNumber(IXLCell flatInSheet)
+        {
+            var flatNumber = Regex.Match(flatInSheet.GetValue<string>(), @"\d+").Value;
+            return flatNumber;
+        }
+
+        // Извлекает и возвращает цену квартиры
+        private string GetPrice(IXLCell flatInSheet)
+        {
+            var price = sheet.Cell(flatInSheet.WorksheetRow().RowNumber() + 1, flatInSheet.WorksheetColumn().ColumnNumber()).GetValue<string>();
+            return price;
+        }
+    }
+}
---
+++ NmarketTestTask/Parsers/HtmlParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class HtmlParser : IParser
+    {
+        private List<House> houses;
+
+        public IList<House> GetHouses(string path)
+        {
+            houses = new List<House>();
+
+            var doc = new HtmlDocument();
+            doc.Load(path, Encoding.GetEncoding("windows-1251"), false);
+
+            // Извлекает таблицу с данными без заголовков
+            var tableData = doc.DocumentNode.SelectSingleNode("//tbody").SelectNodes(".//tr");
+            ProcessRows(tableData);
+
+            return houses;
+        }
+
+        // Проходит через каждую строку в таблице
+        // Для каждой строчки извлекает имя дома используя имя класса СSS и каритру
+        // Также для каждой строчки извлекает заполненный данными объект класса Flat
+        // Добавляет квартиру в дом с именем извлеченным ранее
+        private void ProcessRows(HtmlNodeCollection tableData)
+        {
+            foreach (var row in tableData)
+            {
+                var houseName = row.SelectSingleNode(".//td[@class='house']").InnerText;
+                var flat = GetFlat(row);                
+                AddFlat(flat, houseName);
+            }
+        }
+
+        // Принимает строку из таблицы 
+        // Извлекает данные о цене и номере квартиры используя имена класcов СSS
+        // Возвращает заполненный данными объект Flat
+        private Flat GetFlat(HtmlNode row)
+        {
+            Flat flat = new Flat()
+            {
+                Number = row.SelectSingleNode(".//td[@class='number']").InnerText,
+                Price = row.SelectSingleNode(".//td[@class='price']").InnerText
+            };
+
+            return flat;
+        }
+
+        // Принимает заполненный данными объект Flat и имя дома
+        // Если имя дома отсутсвиет в переменной houses создает новый объект класса House и добовляет его
+        // Если имя дома присутсвует в переменной houses, находит дом и добавляет объект Flaт
+        private void AddFlat(Flat flat, string houseName)
+        {
+            if (houses.Any(h => h.Name == houseName))
+            {
+                houses.Find(h => h.Name == houseName).Flats.Add(flat);
+            }
+            else
+            {
+                houses.Add(new House() { Name = houseName, Flats = new List<Flat>() });
+                houses.Find(h => h.Name == houseName).Flats.Add(flat);
+            }
+
+            
+        }
+    }
+}
